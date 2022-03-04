3eb58456d14983d2b407fca3e4085629fde9902e
NguyenTraVi <19522512@gm.uit.edu.vn>
2022-03-04 09:46:38 +0700

    bug show sceen

---
+++ app/src/main/java/com/nt118/joliecafe/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.nt118.joliecafe.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nt118.joliecafe.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
