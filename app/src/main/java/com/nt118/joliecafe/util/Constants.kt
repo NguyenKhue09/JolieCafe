4b3fa13db198cc28258309b5d9d0648d5db37d54
NguyenKhue09 <19521712@gm.uit.edu.vn>
2022-03-08 16:58:20 +0700

    compeleted ui favorite

--- app/src/main/java/com/nt118/joliecafe/util/Constants.kt
+++ app/src/main/java/com/nt118/joliecafe/util/Constants.kt
@@ -3,6 +3,6 @@ package com.nt118.joliecafe.util
 class Constants {
 
     companion object {
-
+        val listTabContentFavorite = listOf("All", "Coffee", "Tea", "Juice", "Pasty")
     }
-}
+}       
