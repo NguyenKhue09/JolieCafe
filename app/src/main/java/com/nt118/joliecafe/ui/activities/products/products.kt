6b8e05cbe903d610ecf52932460de1b53407ebb5
NguyenTraVi <19522512@gm.uit.edu.vn>
2022-03-22 20:09:53 +0700

    products

--- app/src/main/java/com/nt118/joliecafe/ui/activities/products/products.kt
+++ app/src/main/java/com/nt118/joliecafe/ui/activities/products/products.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.nt118.joliecafe.R
 import com.nt118.joliecafe.databinding.ActivityLoginBinding
 import com.nt118.joliecafe.databinding.ActivityProductsBinding
+import com.nt118.joliecafe.models.CategorieModel
 
 class products : AppCompatActivity() {
     private var _binding: ActivityProductsBinding? = null
@@ -17,6 +18,25 @@ class products : AppCompatActivity() {
 
         val bundle : Bundle? = intent.extras
         val position = bundle!!.getInt("position")
-        binding.tvTest.text = position.toString()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun fetData() : ArrayList<CategorieModel>{
+        val item = ArrayList<CategorieModel>()
+
+        item.add(CategorieModel("All",R.drawable.ic_star))
+        item.add(CategorieModel("Coffee",R.drawable.ic_coffee))
+        item.add(CategorieModel("Tea",R.drawable.ic_leaf))
+        item.add(CategorieModel("Juice",R.drawable.ic_watermelon))
+        item.add(CategorieModel("Pastry",R.drawable.ic_croissant_svgrepo_com))
+        item.add(CategorieModel("Coffee",R.drawable.ic_coffee))
+        item.add(CategorieModel("Coffee",R.drawable.ic_coffee))
+        item.add(CategorieModel("More",R.drawable.ic_coffee))
+
+        return  item
     }
 }
