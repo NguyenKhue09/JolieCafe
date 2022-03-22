e36fbb0544ae2acac42bd56a2936661d1f539949
Nguyen Huu Long <longnh.0601@gmail.com>
2022-03-22 23:45:37 +0700

    add checkout screen

--- app/src/main/java/com/nt118/joliecafe/ui/fragments/cart/CartFragment.kt
+++ app/src/main/java/com/nt118/joliecafe/ui/fragments/cart/CartFragment.kt
@@ -1,14 +1,17 @@
 package com.nt118.joliecafe.ui.fragments.cart
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.nt118.joliecafe.databinding.FragmentCartBinding
+import com.nt118.joliecafe.ui.activities.checkout.CheckoutActivity
 import com.nt118.joliecafe.viewmodels.CartViewModel
 
 class CartFragment : Fragment() {
@@ -36,6 +39,12 @@ class CartFragment : Fragment() {
 //        }
 
         val rvCartSuggestion: RecyclerView = binding.rvCartSuggestion
+        val btnCheckout: Button = binding.btnCheckout
+
+        btnCheckout.setOnClickListener {
+            startActivity(Intent(context, CheckoutActivity::class.java))
+        }
+
         rvCartSuggestion.adapter = CartSuggestionAdapter()
         return root
     }
