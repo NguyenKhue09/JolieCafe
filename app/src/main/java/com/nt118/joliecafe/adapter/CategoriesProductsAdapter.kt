0e4beafc571dae4b42fc0fba8e83c016362f0cfa
NguyenTraVi <19522512@gm.uit.edu.vn>
2022-05-06 00:12:38 +0700

    fix ui

--- app/src/main/java/com/nt118/joliecafe/adapter/CategoriesProductsAdapter.kt
+++ app/src/main/java/com/nt118/joliecafe/adapter/CategoriesProductsAdapter.kt
@@ -1,4 +1,38 @@
 package com.nt118.joliecafe.adapter
 
-class CategoriesProductsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.nt118.joliecafe.databinding.ItemRvCategoriesProductsBinding
+import com.nt118.joliecafe.models.CategorieModel
+
+class CategoriesProductsAdapter(private val item : ArrayList<CategorieModel>) : RecyclerView.Adapter<CategoriesProductsAdapter.ViewHolder>() {
+
+    class ViewHolder(var binding: ItemRvCategoriesProductsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemRvCategoriesProductsBinding.inflate(layoutInflater)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+         return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.imgCategoriesProducts.setImageResource(item[position].image)
+        holder.binding.tvCategoriesProducts.text = item[position].title
+    }
+
+    override fun getItemCount(): Int {
+        return item.size
+    }
 }
